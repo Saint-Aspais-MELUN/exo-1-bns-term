19279980090f3a6dd3e7e9da6542075f5556daa9
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-22 21:56:22 +0100

    Update test_exercice2.py

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,4 +1,10 @@
 from exercices.exercice2 import *
 
-def test_tri_iteratif():
-    assert tri_iteratif([41, 55, 21, 18, 12, 6, 25]) == [6, 12, 18, 21, 25, 41, 55]
+from math import sqrt   # import de la fonction racine carrée
+
+
+def test_distance():
+    assert distance((1, 0), (5, 3)) == 5.0, "erreur de calcul"
+
+def test_plus_courte_distance():
+    assert plus_courte_distance([(7, 9), (2, 5), (5, 2)], (0, 0)) == (2, 5), "erreur"
