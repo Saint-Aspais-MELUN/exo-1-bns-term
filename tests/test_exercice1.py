b65ec8f428433c56ecc66bf35f2a9fb41a416499
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-22 21:53:33 +0100

    Update test_exercice1.py

--- tests/test_exercice1.py
+++ tests/test_exercice1.py
@@ -1,16 +1,6 @@
 from exercices.exercice1 import *
-
-def test_taille():
+def test_recherche():
     ## Ici le code d'initialisation si nécessaire
-    a = {
-        'F':['B','G'],
-        'B':['A','D'],
-        'A':['',''],
-        'D':['C','E'],
-        'C':['',''],
-        'E':['',''],
-        'G':['','I'],
-        'I':['','H'],
-        'H':['','']
-    }
-    assert taille(a,'F') == 9
+    assert recherche([5, 3],1) == 2
+    assert recherche([2,4],2) == 0
+    assert recherche([2,3,5,2,4],2) == 3
